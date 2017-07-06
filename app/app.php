ae59c48dbb85fc9f2a58e654047d66782cd82cc6
jacob ruleaux and Michelle Poterek <jacob@>
2017-07-06 15:07:42 -0700

    gittin twiggy with it. Calla style

--- app/app.php
+++ app/app.php
@@ -3,8 +3,12 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Scrabble.php";
 
-    $app = new Silex\Application();
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
 
+
+    $app = new Silex\Application();
+    $app['debug'] = true;
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
@@ -13,5 +17,10 @@
         return $app['twig']->render('form.html.twig');
     });
 
+    $app->get("/score", function() use ($app){
+        $finalScore = new Scrabble;
+        $the_outcome = $finalScore->scrabbleAtor($_GET['word']);
+        return $app['twig']->render('score.html.twig', array('result' => $the_outcome));
+    });
     return $app;
 ?>
--- src/Scrabble.php
+++ src/Scrabble.php
@@ -38,25 +38,9 @@
             $totalScore = array_sum($array_of_letters);
             return $totalScore;
             var_dump($totalScore);
-
-
-            // return $word_split;
         }
 
 
 
-        // // function scoreAtor($word)
-        // // {
-        //     // $points = array();
-        //     // $points = "1";
-        //     // $one_point = array("A" => 1, "E" => 1, "I" => 1, "O" => 1, "U" => 1, "L" => 1, "N" => 1, "R" => 1, "S" => 1, "T" => 1);
-        //     foreach($word as $letter)
-        //     {
-        //         if($letter == "A") {
-        //             // array_push($points);
-        //         }
-        //     return "1";
-        //     }
-         }
-    // }
- ?>
+    }
+?>
